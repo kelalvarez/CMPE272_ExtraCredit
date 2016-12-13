9bb8c43fe5eb65aacb4cfa04c140fea9bc9f7bd4
Kelly Alvarez <kalvarez@klinche.com>
2016-12-13 01:41:56 -0800

    Set project

---
+++ iOS-Project/cameraAction/Pods/Target Support Files/Pods-cameraAction/Pods-cameraAction-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_cameraActionVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_cameraActionVersionString[];
+
